f718de59e63f3dc2ecf410ab6bdcd43e553ee208
Tyler Compton <xaviosx@gmail.com>
2019-05-12 11:41:33 -0700

    Update more instructions to be M-Cycle accurate
    - CB-Prefix instructions updated
    - Call/return instructions updated

--- gameboy/bit_ops.go
+++ gameboy/bit_ops.go
@@ -3,60 +3,82 @@ package gameboy
 // makeRES creates an instruction that sets the specified bit of the given
 // register to zero.
 func makeRES(bitNum uint8, reg register8) instruction {
-	return adapter(func(state *State) int {
+	return func(state *State) instruction {
+		// M-Cycle 1: Fetch instruction and do operation
+
 		reg.set(reg.get() & ^(0x1 << bitNum))
 
-		return 8
-	})
+		return nil
+	}
 }
 
 // makeRESMemHL creates an instruction that sets the specified bit of the value
 // at the address specified by register HL to zero.
 func makeRESMemHL(bitNum uint8) instruction {
-	return adapter(func(state *State) int {
-		memVal := state.mmu.at(state.regHL.get())
+	return func(state *State) instruction {
+		// M-Cycle 1: Fetch instruction
 
-		memVal &= ^(0x1 << bitNum)
-		state.mmu.set(state.regHL.get(), memVal)
+		return func(state *State) instruction {
+			// M-Cycle 2: Read from memory
 
-		return 16
-	})
+			memVal := state.mmu.at(state.regHL.get())
+
+			return func(state *State) instruction {
+				// M-Cycle 3: Do operation and write to memory
+
+				memVal &= ^(0x1 << bitNum)
+				state.mmu.set(state.regHL.get(), memVal)
+
+				return nil
+			}
+		}
+	}
 }
 
 // makeBIT creates an instruction that checks the given bit of the given
 // register value.
 func makeBIT(bitNum uint8, reg register8) instruction {
-	return adapter(func(state *State) int {
+	return func(state *State) instruction {
+		// M-Cycle 1: Fetch instruction and do operation
+
 		bitSet := reg.get()&(0x1<<bitNum) == (0x1 << bitNum)
 
 		state.setZeroFlag(!bitSet)
 		state.setSubtractFlag(false)
 		state.setHalfCarryFlag(true)
 
-		return 8
-	})
+		return nil
+	}
 }
 
 // makeBITMemHL creates an instruction that checks the given bit of the value
 // at the address specified by register HL.
 func makeBITMemHL(bitNum uint8) instruction {
-	return adapter(func(state *State) int {
-		memVal := state.mmu.at(state.regHL.get())
+	return func(state *State) instruction {
+		// M-Cycle 1: Fetch instruction
 
-		bitSet := memVal&(0x1<<bitNum) == (0x1 << bitNum)
+		return func(state *State) instruction {
+			// M-Cycle 2: Read from memory and do operation
 
-		state.setZeroFlag(!bitSet)
-		state.setSubtractFlag(false)
-		state.setHalfCarryFlag(true)
+			memVal := state.mmu.at(state.regHL.get())
 
-		return 16
-	})
+			bitSet := memVal&(0x1<<bitNum) == (0x1 << bitNum)
+
+			state.setZeroFlag(!bitSet)
+			state.setSubtractFlag(false)
+			state.setHalfCarryFlag(true)
+
+			return nil
+		}
+	}
 }
 
 // makeSWAP creates an instruction that swaps the upper and lower nibbles of
 // the given register.
 func makeSWAP(reg register8) instruction {
-	return adapter(func(state *State) int {
+	return func(state *State) instruction {
+		// M-Cycle 1: Do operation
+
 		lower, upper := split(reg.get())
 		reg.set(combine(upper, lower))
 
@@ -65,46 +87,69 @@ func makeSWAP(reg register8) instruction {
 		state.setHalfCarryFlag(false)
 		state.setCarryFlag(false)
 
-		return 8
-	})
+		return nil
+	}
 }
 
 // swapMemHL swaps the upper and lower nibbles of the value in memory at the
 // address specified by register HL.
-func swapMemHL(state *State) int {
-	memVal := state.mmu.at(state.regHL.get())
+func swapMemHL(state *State) instruction {
+	// M-Cycle 1: Fetch instruction
 
-	lower, upper := split(memVal)
-	memVal = combine(upper, lower)
-	state.mmu.set(state.regHL.get(), memVal)
+	return func(state *State) instruction {
+		// M-Cycle 2: Read from memory
 
-	state.setZeroFlag(memVal == 0)
-	state.setSubtractFlag(false)
-	state.setHalfCarryFlag(false)
-	state.setCarryFlag(false)
+		memVal := state.mmu.at(state.regHL.get())
 
-	return 16
+		return func(state *State) instruction {
+			// M-Cycle 3: Do operation and write to memory
+
+			lower, upper := split(memVal)
+			memVal = combine(upper, lower)
+			state.mmu.set(state.regHL.get(), memVal)
+
+			state.setZeroFlag(memVal == 0)
+			state.setSubtractFlag(false)
+			state.setHalfCarryFlag(false)
+			state.setCarryFlag(false)
+
+			return nil
+		}
+	}
+
 }
 
 // makeSET creates an instruction that sets the specified bit of the given
 // register to one.
 func makeSET(bitNum uint8, reg register8) instruction {
-	return adapter(func(state *State) int {
+	return func(state *State) instruction {
+		// M-Cycle 1: Fetch instruction and do operation
+
 		reg.set(reg.get() | 0x1<<bitNum)
 
-		return 8
-	})
+		return nil
+	}
 }
 
 // makeSETMemHL creates an instruction that sets the specified bit of the value
 // at the address specified by register HL to one.
 func makeSETMemHL(bitNum uint8) instruction {
-	return adapter(func(state *State) int {
-		memVal := state.mmu.at(state.regHL.get())
+	return func(state *State) instruction {
+		// M-Cycle 1: Fetch instruction
 
-		memVal |= 0x1 << bitNum
-		state.mmu.set(state.regHL.get(), memVal)
+		return func(state *State) instruction {
+			// M-Cycle 2: Read from memory
 
-		return 16
-	})
+			memVal := state.mmu.at(state.regHL.get())
+
+			return func(state *State) instruction {
+				// M-Cycle 3: Write to memory
+
+				memVal |= 0x1 << bitNum
+				state.mmu.set(state.regHL.get(), memVal)
+
+				return nil
+			}
+		}
+	}
 }
--- gameboy/call_return_ops.go
+++ gameboy/call_return_ops.go
@@ -2,80 +2,233 @@ package gameboy
 
 // call loads a 16-bit address, pushes the address of the next instruction onto
 // the stack, and jumps to the loaded address.
-func call(state *State) int {
-	address := combine16(state.incrementPC(), state.incrementPC())
-	state.pushToStack16(state.regPC.get())
+func call(state *State) instruction {
+	// M-Cycle 0: Fetch instruction
 
-	state.regPC.set(address)
+	return func(state *State) instruction {
+		// M-Cycle 1: Read lower byte of destination address
 
-	return 24
+		addressLower := state.incrementPC()
+
+		return func(state *State) instruction {
+			// M-Cycle 2: Read upper byte of destination address
+
+			addressUpper := state.incrementPC()
+
+			return func(state *State) instruction {
+				// M-Cycle 3: Some unknown internal operation
+
+				return func(state *State) instruction {
+					// M-Cycle 4: Push the upper byte of the current PC onto
+					//            the stack
+
+					_, pcUpper := split16(state.regPC.get())
+					state.pushToStack(pcUpper)
+
+					return func(state *State) instruction {
+						// M-Cycle 5: Push the lower byte of the current PC
+						//            onto the stack and set the PC to the
+						//            target address
+
+						pcLower, _ := split16(state.regPC.get())
+						state.pushToStack(pcLower)
+
+						// TODO(velovix): Is this actually where the PC is set?
+						state.regPC.setLower(addressLower)
+						state.regPC.setUpper(addressUpper)
+
+						return nil
+					}
+				}
+			}
+		}
+
+	}
 }
 
 // makeCALLIfFlag creates an instruction that loads a 16-bit address, pushes
 // the address of the next instruction onto the stack, and jumps to the loaded
 // address if the given flag is at the expected setting.
 func makeCALLIfFlag(flagMask uint8, isSet bool) instruction {
-	return adapter(func(state *State) int {
-		flagState := state.regF.get()&flagMask == flagMask
-		address := combine16(state.incrementPC(), state.incrementPC())
+	return func(state *State) instruction {
+		// M-Cycle 0: Fetch instruction
 
-		if flagState == isSet {
-			state.pushToStack16(state.regPC.get())
-			state.regPC.set(address)
+		return func(state *State) instruction {
+			// M-Cycle 1: Read lower byte of destination address
 
-			return 24
+			addressLower := state.incrementPC()
+
+			return func(state *State) instruction {
+				// M-Cycle 2: Read upper byte of destination address and check
+				//            the flag value
+
+				addressUpper := state.incrementPC()
+
+				flagState := state.regF.get()&flagMask == flagMask
+
+				if flagState != isSet {
+					// Condition evaluated to false, don't jump to the target
+					// address
+					return nil
+				}
+
+				return func(state *State) instruction {
+					// M-Cycle 3: Some unknown internal operation
+
+					return func(state *State) instruction {
+						// M-Cycle 4: Push the upper byte of the current PC
+						//            onto the stack
+
+						_, pcUpper := split16(state.regPC.get())
+						state.pushToStack(pcUpper)
+
+						return func(state *State) instruction {
+							// M-Cycle 5: Push the lower byte of the current PC
+							//            onto the stack and set the PC to the
+							//            target address
+
+							pcLower, _ := split16(state.regPC.get())
+							state.pushToStack(pcLower)
+
+							// TODO(velovix): Is this actually where the PC is set?
+							state.regPC.setLower(addressLower)
+							state.regPC.setUpper(addressUpper)
+
+							return nil
+						}
+					}
+				}
+			}
 		}
-		// A call didn't happen, so the instruction took fewer cycles
-		return 12
-	})
+	}
 }
 
 // ret pops a 16-bit address from the stack and jumps to it.
-func ret(state *State) int {
-	addr := state.popFromStack16()
-	state.regPC.set(addr)
+func ret(state *State) instruction {
+	// M-Cycle 0: Fetch instruction
 
-	return 16
+	return func(state *State) instruction {
+		// M-Cycle 1: Pop the lower byte of the target address off the stack
+
+		addressLower := state.popFromStack()
+
+		return func(state *State) instruction {
+			// M-Cycle 2: Pop the upper byte of the target address off the
+			//            stack
+
+			addressUpper := state.popFromStack()
+
+			return func(state *State) instruction {
+				// M-Cycle 3: Probably set the PC to the target address
+
+				state.regPC.setLower(addressLower)
+				state.regPC.setUpper(addressUpper)
+
+				return nil
+			}
+		}
+	}
 }
 
 // makeRETIfFlag creates an instruction that pops a 16-bit address from the
 // stack and jumps to it, but only if the given flag is at the expected value.
 func makeRETIfFlag(flagMask uint8, isSet bool) instruction {
-	return adapter(func(state *State) int {
-		flagState := state.regF.get()&flagMask == flagMask
+	return func(state *State) instruction {
+		// M-Cycle 0: Fetch instruction
 
-		var opClocks int
-		if flagState == isSet {
-			addr := state.popFromStack16()
-			state.regPC.set(addr)
-			opClocks = 20
-		} else {
-			opClocks = 8
+		return func(state *State) instruction {
+			// M-Cycle 1: Check flag value
+
+			flagState := state.regF.get()&flagMask == flagMask
+
+			if flagState != isSet {
+				// Condition evaluated to false, don't jump to the target
+				// address
+				return nil
+			}
+
+			return func(state *State) instruction {
+				// M-Cycle 2: Read the lower byte of the target address
+
+				addressLower := state.popFromStack()
+
+				return func(state *State) instruction {
+					// M-Cycle 3: Read the upper byte of the target address
+
+					addressUpper := state.popFromStack()
+
+					return func(state *State) instruction {
+						// M-Cycle 4: Set the PC to the target address
+
+						state.regPC.setLower(addressLower)
+						state.regPC.setUpper(addressUpper)
+
+						return nil
+					}
+				}
+			}
 		}
-
-		return opClocks
-	})
+	}
 }
 
 // reti pops a 16-bit address from the stack and jumps to it, then enables
 // interrupts.
-func reti(state *State) int {
-	addr := state.popFromStack16()
-	state.regPC.set(addr)
+func reti(state *State) instruction {
+	// M-Cycle 0: Fetch instruction
 
-	state.interruptsEnabled = true
+	return func(state *State) instruction {
+		// M-Cycle 1: Read lower byte of target address
 
-	return 16
+		addressLower := state.popFromStack()
+
+		return func(state *State) instruction {
+			// M-Cycle 2: Read upper byte of target address
+
+			addressUpper := state.popFromStack()
+
+			return func(state *State) instruction {
+				// M-Cycle 3: Set the PC to the target address and turn on
+				//            interrupts
+
+				state.regPC.setLower(addressLower)
+				state.regPC.setUpper(addressUpper)
+
+				state.interruptsEnabled = true
+
+				return nil
+			}
+		}
+	}
 }
 
 // makeRST creates an instruction that pushes the current program counter to
 // the stack and jumps to the given address.
 func makeRST(address uint16) instruction {
-	return adapter(func(state *State) int {
-		state.pushToStack16(state.regPC.get())
+	return func(state *State) instruction {
+		// M-Cycle 0: Fetch instruction
 
-		state.regPC.set(address)
+		return func(state *State) instruction {
+			// M-Cycle 1: Some unknown internal behavior
 
-		return 16
-	})
+			return func(state *State) instruction {
+				// M-Cycle 2: Push the upper byte of the PC to the stack
+
+				_, upper := split16(state.regPC.get())
+				state.pushToStack(upper)
+
+				return func(state *State) instruction {
+					// M-Cycle 3: Push the lower byte of the PC to the stack
+					//            and set PC to target address
+
+					lower, _ := split16(state.regPC.get())
+					state.pushToStack(lower)
+
+					// TODO(velovix): Is this where the PC is actually set?
+					state.regPC.set(address)
+
+					return nil
+				}
+			}
+		}
+	}
 }
